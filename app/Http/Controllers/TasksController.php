1829bf82f81b22941b5ced7822dbe20f9e9ebada
Tatsumiya <black.r.d@icloud.com>
2020-04-21 10:38:39 +0000

    test

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-             'status' => 'required|max:191',
+             'status' => 'required|max:10',
             'content' => 'required|max:191']);
         
         
@@ -94,6 +94,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+                $this->validate($request, [
+                    'status' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
+        
         $task = Task::find($id);
         $task->status = $request->status;
 
