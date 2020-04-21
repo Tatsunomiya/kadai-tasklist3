ecc9d2f8d35caf4b230f4f64cfb675b432c7a370
Tatsumiya <black.r.d@icloud.com>
2020-04-21 06:45:32 +0000

    test

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク新規作成ページ</h1>
+    
+    <div class="row">
+        
+        <div class="col-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content','タスク') !!}
+                    {!! Form::text('content',null, ['class' => 'form-contorl']) !!}
+                    
+                        <div class="form-group">
+                            {!! Form::label('content', 'メッセージ:') !!}
+                            {!! Form::text('content',null,['class' => 'form-contorl']) !!}
+                            
+                        </div>
+                        
+                        
+                        {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
+                        
+                    {!! Form::close() !!}
+                    
+                </div>
+        </div>
+
+    
+    
+
+
+
+@endsection
